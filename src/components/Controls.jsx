a7aba9f00c35f8ca7d428ab7b3a848115bfdf2fa
--replace-all <diaconu.codrin@gmail.com>
2016-06-12 16:13:38 -0400

    Completed Countdown with controls and styles.

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 
 class Controls extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+
+        this.onStatusChange = this.onStatusChange.bind(this);
+    }
+
+    onStatusChange(newStatus) {
+        this.props.onStatusChange(newStatus);
+    }
+
     render() {
         const {countdownStatus} = this.props;
         const renderStartStopBtn = () => {
             if(countdownStatus === 'started'){
-                return (<button className="button secondary">Pause</button>);
+                return (<button onClick={this.onStatusChange.bind(null, 'paused')} className="button secondary">Pause</button>);
             } else if (countdownStatus === 'paused'){
-                return (<button className="button primary">Start</button>);
+                return (<button onClick={this.onStatusChange.bind(null, 'started')} className="button primary">Start</button>);
             }
         };
         return (
             <div className="controls">
                 {renderStartStopBtn()}
-                <button className="button alert">Clear</button>
+                <button onClick={this.onStatusChange.bind(null, 'stopped')} className="button alert">Clear</button>
             </div>
         );
     }
 }
 
 Controls.propTypes = {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
 };
 Controls.defaultProps = {};
 
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import Clock from 'Clock';
 import CountdownForm from 'CountdownForm';
 import Controls from 'Controls';
+
+
 class Countdown extends React.Component {
 
     constructor() {
@@ -13,7 +16,10 @@ class Countdown extends React.Component {
         };
 
         this.startTimer = this.startTimer.bind(this);
+        this.pauseTimer = this.pauseTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
         this.handleSetCountdown = this.handleSetCountdown.bind(this);
+        this.handleStatusChange = this.handleStatusChange.bind(this);
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -22,15 +28,30 @@ class Countdown extends React.Component {
                 case 'started':
                     this.startTimer();
                     break;
+                case 'paused':
+                    this.pauseTimer();
+                    break;
+                case 'stopped':
+                    this.resetTimer();
+                    break;
             }
         }
     }
 
     startTimer(){
-        setInterval(() => {
+        this.timer = setInterval(() => {
             let secondsRemaining = this.state.totalSeconds - 1;
             this.setState({totalSeconds: secondsRemaining >= 0 ? secondsRemaining : 0});
         }, 1000);
+    }
+
+    pauseTimer() {
+        clearInterval(this.timer);
+    }
+
+    resetTimer() {
+        clearInterval(this.timer);
+        this.setState({totalSeconds: 0});
     }
 
     handleSetCountdown(totalSeconds) {
@@ -40,12 +61,24 @@ class Countdown extends React.Component {
         });
     }
 
+    handleStatusChange(countdownStatus) {
+        this.setState({countdownStatus});
+    }
+
     render() {
+        const {countdownStatus} = this.state;
+        const renderControls = () => {
+            if(countdownStatus !== 'stopped'){
+                return (<Controls countdownStatus={this.state.countdownStatus} onStatusChange={this.handleStatusChange}/>);
+            } else {
+                return (<CountdownForm onSetCountdown={this.handleSetCountdown}/>);
+            }
+        };
+
         return (
             <div>
                 <Clock totalSeconds={this.state.totalSeconds}/>
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
-                <Controls countdownStatus='paused'/>
+                {renderControls()}
             </div>
         );
     }
